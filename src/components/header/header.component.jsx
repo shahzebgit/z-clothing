11ba49611440dde6b37995c1627baa1421f4b8ac
shahzebgit <shahzebparkar@gmail.com>
2020-05-05 16:45:36 +0530

    Update header.component.jsx

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 import { connect } from "react-redux";
-
 import { createStructuredSelector } from "reselect";
+
 import { auth } from "../../firebase/firebase.util";
 import CartIcon from "../cart-icon/cart-icon.component";
-
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+
 import { selectCartHidden } from "../../redux/cart/cart.selectors";
 import { selectCurrentUser } from "../../redux/user/user.selector";
 
