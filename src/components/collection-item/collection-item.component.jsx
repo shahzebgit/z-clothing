47747dc74399b98fadcdce3632c9feadf4493b16
shahzebgit <shahzebparkar@gmail.com>
2020-05-05 16:45:44 +0530

    Update collection-item.component.jsx

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import CustomButton from '../../components/custom-button/custom-button';
+import CustomButton from '../custom-button/custom-button';
 import { addItem } from '../../redux/cart/cart.action';
 
 import './collection-item.styles.scss';
