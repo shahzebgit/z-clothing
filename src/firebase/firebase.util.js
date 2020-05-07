8a62de07ee67d8842e34e67caa75e01693b21cd8
shahzebgit <shahzebparkar@gmail.com>
2020-05-07 16:20:39 +0530

    Update firebase.util.js

--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -17,6 +17,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
   const userRef = firestore.doc(`users/${userAuth.uid}`);
+
   const snapShot = await userRef.get();
 
   if (!snapShot.exists) {
@@ -37,7 +38,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   return userRef;
 }
 
-   firebase.initializeApp(config);
+  firebase.initializeApp(config);
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
 
