88580f824a26e632fa952b783f299f4ea84a7535
shahzebgit <shahzebparkar@gmail.com>
2020-05-08 15:15:55 +0530

    fucntion for collection and tranform the collection
    added a new function for collections so we can add shop data to our firebase and transform that data to our shop page just like before

--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -38,14 +38,48 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   return userRef;
 }
 
-  firebase.initializeApp(config);
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+  const collectionRef = firestore.collection(collectionKey);
 
-  const provider = new firebase.auth.GoogleAuthProvider();
 
-  provider.setCustomParameters({ prompt: 'select_account' });
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+   batch.set(newDocRef, obj);
 
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+  });
 
-  export default firebase;
+  return await batch.commit()
+};
+
+export const convertCollectionSnapshotToMap =(collections)=>{
+  const transformedCollection = collections.docs.map(doc =>{
+    const {title, items} = doc.data();
+    
+    return{
+      routeName: encodeURI(title.toLowerCase()),
+      id:doc.id,
+      title,
+      items
+    }
+  });
+ return transformedCollection.reduce((accumulator, collection) =>{
+    accumulator[collection.title.toLowerCase()]= collection;
+    return accumulator;
+  },{});
+  
+}
+
+
+
+firebase.initializeApp(config);
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+provider.setCustomParameters({ prompt: 'select_account' });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
