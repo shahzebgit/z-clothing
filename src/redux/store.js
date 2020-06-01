01f27448375e11599ad69c8dad0cd31f5f079c50
shahzebgit <shahzebparkar@gmail.com>
2020-06-01 19:19:26 +0530

    Mounting redux-saga with store page

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,29 +1,34 @@
-import {createStore , applyMiddleware, compose}  from 'redux';
-import { persistStore }  from 'redux-persist';
-import thunk from 'redux-thunk'
+import { createStore, applyMiddleware, compose } from "redux";
+import { persistStore } from "redux-persist";
+// import thunk from "redux-thunk";
 
-import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga'
+import {fetchCollectionsStart} from './shop/shop.sagas'
+
+import logger from "redux-logger";
+import rootReducer from "./root-reducer";
 
 
-import rootReducer from './root-reducer';
+const sagaMiddleware = createSagaMiddleware();
 
-const middlewares = [thunk];
+const middlewares = [sagaMiddleware];
 
-if(process.env.NODE_ENV=== 'development'){
-  middlewares.push(logger); 
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
 }
 const composeEnhancers =
-  typeof window === 'object' &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-    }) : compose;
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
 
- const enhancer = composeEnhancers(
-        applyMiddleware(...middlewares),
-        // other store enhancers if any
-      );
+const enhancer = composeEnhancers(
+  applyMiddleware(...middlewares)
+  // other store enhancers if any
+);
 
-export const store = createStore(rootReducer,enhancer)
+
+export const store = createStore(rootReducer, enhancer);
+sagaMiddleware.run(fetchCollectionsStart);
 export const persistor = persistStore(store);
-
