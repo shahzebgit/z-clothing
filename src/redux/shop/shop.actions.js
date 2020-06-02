55b5f5c63be2f41482c55aa97b0cbd7b0d64b336
shahzebgit <shahzebparkar@gmail.com>
2020-06-02 20:48:33 +0530

    Update shop.actions.js

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,31 +1,35 @@
-import ShopActionTypes from './shop.types';
+import ShopActionTypes from "./shop.types";
 
+import {
+  firestore,
+  convertCollectionSnapshotToMap,
+} from "../../firebase/firebase.util";
 
-import { firestore ,convertCollectionSnapshotToMap} from '../../firebase/firebase.util'
-
-export const fetchCollectionsStart =(collectionsMap)=>({
-    type:ShopActionTypes.FETCH_COLLECTIONS_START,
+export const fetchCollectionsStart = (collectionsMap) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_START,
 });
 
-
-export const fetchCollectionsSuccess = collectionMap =>({
-   type:ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
-   payload: collectionMap
+export const fetchCollectionsSuccess = (collectionMap) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+  payload: collectionMap,
 });
 
-export const fetchCollectionsFailure = errorMessage =>({
-    type:ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
-    payload: errorMessage
-})
+export const fetchCollectionsFailure = (errorMessage) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: errorMessage,
+});
 
-export const fetchCollectionsStartAsync = () =>{
-    return dispatch =>{
-        const collectionRef = firestore.collection('collections'); 
-        dispatch(fetchCollectionsStart());
+export const fetchCollectionsStartAsync = () => {
+  return (dispatch) => {
+    const collectionRef = firestore.collection("collections");
+    dispatch(fetchCollectionsStart());
 
-        collectionRef.get().then( (async snapshot =>{
-            const collectionMap = convertCollectionSnapshotToMap(snapshot);
-            dispatch(fetchCollectionsSuccess(collectionMap));
-          })).catch (error=> dispatch(fetchCollectionsFailure(error.message)));
-    }
-}
+    collectionRef
+      .get()
+      .then(async (snapshot) => {
+        const collectionMap = convertCollectionSnapshotToMap(snapshot);
+        dispatch(fetchCollectionsSuccess(collectionMap));
+      })
+      .catch((error) => dispatch(fetchCollectionsFailure(error.message)));
+  };
+};
