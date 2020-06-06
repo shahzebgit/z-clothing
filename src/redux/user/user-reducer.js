e1f24fb33bad00d0ec0e3c3c2688f60ab82f8b97
shahzebgit <shahzebparkar@gmail.com>
2020-06-06 15:02:42 +0530

    Added user-saga and made changes to user action,reducer,types to get connectivity

--- src/redux/user/user-reducer.js
+++ src/redux/user/user-reducer.js
@@ -1,23 +1,29 @@
-import  {UserActionTypes } from './user.types'
-
+import  UserActionTypes  from "./user.types";
 
 const INITIAL_STATE = {
-    currentUser: null
-}
+  currentUser: null,
+};
 
 const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
 
-    switch (action.type) {
-        case UserActionTypes.SET_CURRENT_USER:
-            return {
-                ...state,
-                currentUser: action.payload
-            }
-        default:
-            return state;
+    case UserActionTypes.GOOGLE_SIGN_IN_SUCESS:
+    case UserActionTypes.EMAIL_SIGN_IN_SUCCESS:
+      return {
+        ...state,
+        currentUser: action.payload,
+        error: null,
+      };
+    case UserActionTypes.GOOGLE_SIGN_IN_FAILURE:
+    case UserActionTypes.EMAIL_SIGN_IN_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+      };
 
-    }
-
-}
+    default:
+      return state;
+  }
+};
 
 export default userReducer;
