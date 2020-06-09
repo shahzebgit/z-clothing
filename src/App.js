382f3fcfd45bb974a42e9ab7088213f0e4de7cc4
shahzebgit <shahzebparkar@gmail.com>
2020-06-09 10:34:52 +0530

    Removed CurrentUser fetching from sagas

--- src/App.js
+++ src/App.js
@@ -10,16 +10,13 @@ import ShopPage from "./pages/shop pages/shop";
 import SignInPage from "./pages/Sign In and Reg/sign-in.component";
 import CheckoutPage from "./pages/checkout/checkout.components";
 
-import { setCurrentUser } from "./redux/user/user-action";
 import { selectCurrentUser } from "./redux/user/user.selector";
-
-import { auth, createUserProfileDocument } from "./firebase/firebase.util";
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
+
 
   //   this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
   //     if (userAuth) {
@@ -66,8 +63,4 @@ const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps)(App);
