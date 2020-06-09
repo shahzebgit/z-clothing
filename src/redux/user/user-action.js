af7093877a5fa05966569a0977e71d90c264c1ff
shahzebgit <shahzebparkar@gmail.com>
2020-06-09 10:38:05 +0530

    Changes in user actions and types ,updating the sagas

--- src/redux/user/user-action.js
+++ src/redux/user/user-action.js
@@ -1,39 +1,24 @@
 import UserActionTypes from '../user/user.types'
 
 
-export const setCurrentUser = user =>({
-    type:UserActionTypes.SET_CURRENT_USER,
-    payload:user
-});
-
-
 export const googleSignInStart =() =>({
     type:UserActionTypes.GOOGLE_SIGN_IN_START
 });
 
 
-export const googleSignInSuccess =(user)=>({
-    type:UserActionTypes.GOOGLE_SIGN_IN_SUCESS,
+export const signInSuccess =(user)=>({
+    type:UserActionTypes.SIGN_IN_SUCCESS,
     payload:user
 })
 
-export const googleSignInFailure =(error)=>({
-    type:UserActionTypes.GOOGLE_SIGN_IN_FAILURE,
+export const signInFailure =(error)=>({
+    type:UserActionTypes.SIGN_IN_FAILURE,
     payload:error
 })
 
-export const emailSignInStart =(emailAndPAassword) =>({
+export const emailSignInStart =(emailAndPassword) =>({
     type:UserActionTypes.EMAIL_SIGN_IN_START,
-    payload:emailAndPAassword
+    payload:emailAndPassword
 });
 
 
-export const emailSignInSuccess =(user)=>({
-    type:UserActionTypes.EMAIL_SIGN_IN_SUCCESS,
-    payload:user
-})
-
-export const emailSignInFailure =(error)=>({
-    type:UserActionTypes.EMAIL_SIGN_IN_FAILURE,
-    payload:error
-})
