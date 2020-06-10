c86cea31bba739e63e4d3a60dc656121ec3fff55
shahzebgit <shahzebparkar@gmail.com>
2020-06-10 15:03:57 +0530

    Added Current user for persistance with Promise

--- src/App.js
+++ src/App.js
@@ -11,33 +11,15 @@ import SignInPage from "./pages/Sign In and Reg/sign-in.component";
 import CheckoutPage from "./pages/checkout/checkout.components";
 
 import { selectCurrentUser } from "./redux/user/user.selector";
+import {checkUserSession}  from './redux/user/user-action'
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-
-
-  //   this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
-  //     if (userAuth) {
-  //       const userRef = await createUserProfileDocument(userAuth);
-
-  //       userRef.onSnapshot((snapShot) => {
-  //         setCurrentUser({
-  //           id: snapShot.id,
-  //           ...snapShot.data(),
-  //         });
-  //       });
-  //     }
-
-  //     setCurrentUser(userAuth);
-  //   });
-  // }
-
-  // componentWillUnmount() {
-  //   this.unsubscribeFromAuth();
+    const {checkUserSession} =this.props;
+    checkUserSession();
   }
-
   render() {
     return (
       <div>
@@ -63,4 +45,8 @@ const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
 });
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = dispatch=>({
+  checkUserSession:()=>dispatch(checkUserSession())
+})
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
