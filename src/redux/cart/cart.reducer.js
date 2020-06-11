b06a6b7e9df6594958e120e9ad8b271dec388ee9
Shahzeb Parkar <shahzebparkar@gmail.com>
2020-06-11 10:00:00 +0530

    Added clear cart functionality
    Added a clear cart method after sign out

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,4 @@
-import CartActionType from './cart.type';
+import CartActionTypes from './cart.type';
 import { addItemToCart ,removeItemFromCart} from './cart.utils'
 
 const INITIAL_STATE ={
@@ -8,25 +8,30 @@ const INITIAL_STATE ={
 
 const cartReducer = (state=INITIAL_STATE,action)=>{
     switch(action.type){
-        case CartActionType.TOGGLE_CART_HIDDEN:
+        case CartActionTypes.TOGGLE_CART_HIDDEN:
             return{
                 ...state,
                 hidden:!state.hidden
             };
-            case CartActionType.ADD_ITEM:
+            case CartActionTypes.ADD_ITEM:
                 return{
                     ...state,
                     cartItems:addItemToCart(state.cartItems,action.payload)
                 }
-            case CartActionType.REMOVE_ITEM:
+            case CartActionTypes.REMOVE_ITEM:
                 return{
                     ...state,
                     cartItems:removeItemFromCart(state.cartItems,action.payload)
                 }
-            case CartActionType.CLEAR_ITEM_FROM_CART:
+            case CartActionTypes.CLEAR_ITEM_FROM_CART:
                 return{
                     ...state,
                     cartItems:state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
+                }
+            case CartActionTypes.CLEAR_CART:
+                return{
+                    ...state,
+                    cartItems:[]
                 }
             default:
                 return state;
