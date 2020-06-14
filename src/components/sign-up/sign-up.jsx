d62494d2def6007a9cef572e636fd9759942aaab
Shahzeb Parkar <shahzebparkar@gmail.com>
2020-06-14 19:27:16 +0530

    added saga functionality to the sign-up

--- src/components/sign-up/sign-up.jsx
+++ src/components/sign-up/sign-up.jsx
@@ -3,9 +3,9 @@ import React from "react";
 import FormInput from "../form-input/form-input";
 import CustomButton from "../custom-button/custom-button";
 
-import { auth, createUserProfileDocument } from "../../firebase/firebase.util";
-
 import "./sign-up.styles.scss";
+import { signUpStart } from "../../redux/user/user-action";
+import { connect } from "react-redux";
 
 class SignUp extends React.Component {
   constructor() {
@@ -21,29 +21,14 @@ class SignUp extends React.Component {
 
   handleSubmit = async (event) => {
     event.preventDefault();
+    const{signUpStart} = this.props;
     const { displayName, email, password, confirmPassword } = this.state;
 
     if (password !== confirmPassword) {
       alert("password doesn't match");
       return;
     }
-
-    try {
-      const { user } = await auth.createUserWithEmailAndPassword(
-        email,
-        password
-      );
-
-      await createUserProfileDocument(user, { displayName });
-      this.setState({
-        displayName: "",
-        email: "",
-        password: "",
-        confirmPassword: "",
-      });
-    } catch (error) {
-      console.log(error);
-    }
+    signUpStart({displayName,email,password})
   };
 
   handleChange = (event) => {
@@ -102,4 +87,8 @@ class SignUp extends React.Component {
   }
 }
 
-export default SignUp;
+const mapDispatchToProps =dispatch=>({
+  signUpStart:(userCredentials)=>dispatch(signUpStart(userCredentials) )
+})
+
+export default connect(null,mapDispatchToProps)(SignUp);
