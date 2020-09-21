af589fbafa0f4f7403a5ee61c93b8c359490a7dc
shahzebgit <shahzebparkar@gmail.com>
2020-09-21 22:20:38 +0530

    Fixed the fix  not working issue

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
 const compression = require("compression");
-cosnt enforce =require('express-sslify')
+const enforce = require("express-sslify");
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
@@ -15,7 +15,7 @@ const port = process.env.PORT || 5000;
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(enforce.HTTPS({trustProtoHeader:true}))
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
