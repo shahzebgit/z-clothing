3a88a2ccdbdf1d4aa7b3653edea5a80dc7cee90e
Shahzeb <shahzebparkar@gmail.com>
2021-08-05 23:15:22 +0530

    No changes

--- client/src/App.js
+++ client/src/App.js
@@ -12,15 +12,15 @@ import { GlobalStyles } from "./global.styles.js";
 import Spinner from "./components/spinner/spinner.component";
 import ErrorBoundary from "./components/error-boundary/error-boundary.component";
 
-const HomePage = lazy(() => import("./pages/homepage/homepage.component"));
-const ShopPage = lazy(() => import("./pages/shop pages/shop.component"));
-const SignInPage = lazy(() => import("./pages/registration-page/registration"));
-const CheckoutPage = lazy(() => import("./pages/checkout/checkout.components"));
+const HomePage = lazy( () => import( "./pages/homepage/homepage.component" ) );
+const ShopPage = lazy( () => import( "./pages/shop pages/shop.component" ) );
+const SignInPage = lazy( () => import( "./pages/registration-page/registration" ) );
+const CheckoutPage = lazy( () => import( "./pages/checkout/checkout.components" ) );
 
-const App = ({ checkUserSession, currentUser }) => {
-  useEffect(() => {
+const App = ( { checkUserSession, currentUser } ) => {
+  useEffect( () => {
     checkUserSession();
-  }, [checkUserSession]);
+  }, [checkUserSession] );
 
   return (
     <div>
@@ -46,12 +46,12 @@ const App = ({ checkUserSession, currentUser }) => {
   );
 };
 
-const mapStateToProps = createStructuredSelector({
+const mapStateToProps = createStructuredSelector( {
   currentUser: selectCurrentUser,
-});
+} );
 
-const mapDispatchToProps = (dispatch) => ({
-  checkUserSession: () => dispatch(checkUserSession()),
-});
+const mapDispatchToProps = ( dispatch ) => ( {
+  checkUserSession: () => dispatch( checkUserSession() ),
+} );
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect( mapStateToProps, mapDispatchToProps )( App );
